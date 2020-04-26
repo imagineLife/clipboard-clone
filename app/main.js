75cbad80d29504cb6acc173c33de83cda6a6e974
Eric Laursen <ericjlaursen@gmail.com>
2020-04-26 16:14:35 -0400

    windows-only startup handlers

--- app/main.js
+++ app/main.js
@@ -1,5 +1,9 @@
 import { app, BrowserWindow } from 'electron';
 
+// Handle creating/removing shortcuts on Windows when installing/uninstalling
+if (require('electron-squirrel-startup')) {
+  app.quit();
+}
 
 /*
 	global reference of the window object,
