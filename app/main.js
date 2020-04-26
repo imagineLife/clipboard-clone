b6fc39ffbc3f176488fee3668e1b590556164574
Eric Laursen <ericjlaursen@gmail.com>
2020-04-26 07:35:14 -0400

    logs a string on app readyc

--- app/main.js
+++ app/main.js
@@ -6,3 +6,15 @@ import { app, BrowserWindow } from 'electron';
 	preventing js from garbage-collecting the mainWindow var;
 */
 let mainWindow;
+
+const createWindow = () => {
+  console.log('-- CREATE WINDOW FN--');
+};
+
+/*
+	...when Electron has finished initialization
+	&&
+	is ready to create browser windows.
+	Some APIs can only be used after this event occurs
+*/
+app.on('ready', createWindow);
