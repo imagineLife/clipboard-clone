0748462fe6704814e27f12302ea7ce3c6d475475
Eric Laursen <ericjlaursen@gmail.com>
2020-04-26 15:08:25 -0400

    clippings state

--- app/frontend/App/App.js
+++ app/frontend/App/App.js
@@ -1,21 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-const App = () => (
-  <main className="container">
-    {/* Header */}
-    <header className="controls">
-      <button id="copy-from-clipboard">
-        Copy From Clipboard
-      </button>
-    </header>
+const App = () => {
+  const [clippings, setClipping] = useState([{ content: 'demo', id: 234 }]);
+  return (
+    <main className="container">
+      {/* Header */}
+      <header className="controls">
+        <button id="copy-from-clipboard">
+          Copy From Clipboard
+        </button>
+      </header>
 
-    {/* Content */}
-    <section className="content">
-      <div className="clipping-list">
-        ...clippings will go here!
-      </div>
-    </section>
-  </main>
-);
+      {/* Content */}
+      <section className="content">
+        <div className="clipping-list">
+          ...clippings will go here!
+        </div>
+      </section>
+    </main>
+  );
+};
 
 export default App;
