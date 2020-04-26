0f0475eeea41ac67005ea9e65c168d1166c0a204
Eric Laursen <ericjlaursen@gmail.com>
2020-04-26 15:11:06 -0400

    addClipping fn

--- app/frontend/App/App.js
+++ app/frontend/App/App.js
@@ -1,12 +1,28 @@
 import React, { useState } from 'react';
+import { clipboard } from 'electron';
 
 const App = () => {
   const [clippings, setClipping] = useState([{ content: 'demo', id: 234 }]);
+
+  const addClipping = () => {
+    const content = clipboard.readText();
+    const id = Date.now();
+
+    const clipping = { id, content };
+
+    setClipping({
+      clippings: [clipping, ...clippings],
+    });
+  };
+
   return (
     <main className="container">
       {/* Header */}
       <header className="controls">
-        <button id="copy-from-clipboard">
+        <button
+          id="copy-from-clipboard"
+          onClick={addClipping}
+        >
           Copy From Clipboard
         </button>
       </header>
