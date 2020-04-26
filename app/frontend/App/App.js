43a766b1698510126f90be03d103bc1697182c75
Eric Laursen <ericjlaursen@gmail.com>
2020-04-26 07:57:29 -0400

    app content structure

--- app/frontend/App/App.js
+++ app/frontend/App/App.js
@@ -1,5 +1,21 @@
 import React from 'react';
 
-const App = () => (<p>App</p>);
+const App = () => (
+  <main className="container">
+    {/* Header */}
+    <header className="controls">
+      <button id="copy-from-clipboard">
+        Copy From Clipboard
+      </button>
+    </header>
+
+    {/* Content */}
+    <section className="content">
+      <div className="clipping-list">
+        ...clippings will go here!
+      </div>
+    </section>
+  </main>
+);
 
 export default App;
