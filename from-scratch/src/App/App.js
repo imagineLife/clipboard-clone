02576d2a5ba8321e79a62c90b4ad3c5f17b48ab8
Eric Laursen <ericjlaursen@gmail.com>
2020-07-28 21:29:48 -0400

    init

---
+++ from-scratch/src/App/App.js
@@ -0,0 +1,103 @@
+import React, {
+  useState,
+  useEffect,
+} from 'react';
+
+import {
+  clipboard,
+  ipcRenderer,
+} from 'electron';
+
+import Clipping from '../Clipping';
+
+
+// DB!!
+import clippingsDB from '../../database';
+
+const App = () => {
+  const [clippings, setClippings] = useState([{ content: 'demo', id: 234 }]);
+  const [clippingEffect, setClippingEffect] = useState(false);
+  const [addToBoardFromApp, setAddToBoardFromApp] = useState(false);
+
+  const writeToClipboard = (c) => {
+    clipboard.writeText(c.content);
+  };
+
+  // const fetchDBClippings = () => {
+  //   clippingsDB('clippings')
+  //     .select()
+  //     .then(setClippings);
+  // };
+
+  // fetch db data!
+  // useEffect(() => {
+  //   fetchDBClippings();
+  // }, []);
+
+  // side-effect to update app from clipboard
+  useEffect(() => {
+    if (clippingEffect) {
+      const content = clipboard.readText();
+      const id = Date.now();
+      const clipping = { id, content };
+
+      // prep new state
+      const newClippings = [clipping, ...clippings];
+
+      // update state
+      setClippings(newClippings);
+      setClippingEffect(false);
+    }
+  }, [clippingEffect]);
+
+  useEffect(() => {
+    if (addToBoardFromApp) {
+      const firstClipping = clippings[0];
+      console.log('firstClipping');
+      console.log(firstClipping);
+
+      if (firstClipping) writeToClipboard(firstClipping);
+      setAddToBoardFromApp(false);
+    }
+  }, [addToBoardFromApp]);
+
+  // register event listeners
+  useEffect(() => {
+  	ipcRenderer.on('create-new-clipping', () => {
+      setClippingEffect(true);
+    });
+
+    ipcRenderer.on('copy-to-board', () => {
+      setAddToBoardFromApp(true);
+    });
+  }, []);
+
+  return (
+    <main className="container">
+      {/* Header */}
+      <header className="controls">
+        <button
+          id="copy-from-clipboard"
+          onClick={() => setClippingEffect(true)}
+        >
+          Copy From Clipboard
+        </button>
+      </header>
+
+      {/* Content */}
+      <section className="content">
+        <div className="clipping-list">
+          {clippings.map((clip, clipIdx) => (
+            <Clipping
+              key={`clipping-${clipIdx}`}
+              content={clip.content}
+              onClick={() => setAddToBoardFromApp(true)}
+            />
+          ))}
+        </div>
+      </section>
+    </main>
+  );
+};
+
+export default App;
