1070d91a29ec76b8da169b9ce7c4911871c91fbb
Eric Laursen <ericjlaursen@gmail.com>
2020-08-01 20:40:40 -0400

    remove + onRemove button

--- from-scratch/src/Clipping/Clipping.js
+++ from-scratch/src/Clipping/Clipping.js
@@ -1,16 +1,24 @@
 import React from 'react';
 import './Clipping.js';
 
-const Clipping = ({ content, onClick }) => (
+const Clipping = ({ content, onClick, clipId, onRemove }) => (
   <article className="clippings-list-item">
     <div className="clipping-text" disabled>
       {content}
     </div>
     <div className="clipping-controls">
+
       <button onClick={() => onClick(content)}>
         &rarr; Clipboard
       </button>
-      <button className="remove-clipping">Remove</button>
+    
+      <button 
+        className="remove-clipping" 
+        onClick={() => onRemove(clipId)}
+      >
+        Remove
+      </button>
+    
     </div>
   </article>
 );
