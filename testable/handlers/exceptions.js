52fa99033649c16c1d6d2531528997c5c48c3eb2
Eric Laursen <ericjlaursen@gmail.com>
2020-08-16 18:17:38 -0400

    handlers

--- testable/handlers/exceptions.js
+++ testable/handlers/exceptions.js
@@ -1,5 +1,20 @@
+// Dependencies
+const path = require('path');
+const wf = require('write-file');
+
+// Variables
+const exceptionsPath = path.join(__dirname, 'uncaughtexceptions');
+
 const exceptionsHandler = (req,res) => {
-	return res.status(200).json({})
+	 const errorFilePath = path.join(exceptionsPath, `${uuid()}.json`);
+  const report = JSON.stringify({ ...req.body, date: new Date() });
+
+  wf(errorFilePath, report, error => {
+    if (error) return console.error('Error Saving', report);
+    console.log('Exception Saved', errorFilePath, report);
+  });
+
+  response.end();
 }
 
-module.exports.exceptionsHandler = exceptionsHandler;
+module.exports = { exceptionsHandler }
