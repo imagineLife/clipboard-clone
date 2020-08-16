251ca0ca28db9335dd785553ee680924d1f0c6a5
Eric Laursen <ericjlaursen@gmail.com>
2020-08-15 21:04:12 -0400

    handlers, endpoints

--- testable/mockServer.js
+++ testable/mockServer.js
@@ -7,6 +7,9 @@ const wf = require('write-file');
 const path = require('path')
 const http = require('http');
 
+// handlers
+const crashReportHandler = require('./handlers/crashReport');
+const exceptionsHandler = require('./exceptions')
 // Vars
 const SERVER_PORT = 8082;
 const crashesPath = path.join(__dirname, 'crashes');
@@ -18,9 +21,12 @@ const httpServer = http.createServer(app);
 // body-parser middleware
 app.use(bp.urlencoded({extended:false}));
 
+// multer middleware
 const multerMW = multer({dest: crashesPath}).single('upload_file_minidump');
 
-
+//endpoints
+app.post('/crashreports', multerMW, crashReportHandler)
+app.post('./uncaughtexceptions', exceptionsHandler)
 
 httpServer.listen(SERVER_PORT, () => {
 	console.log('crash-server running');
