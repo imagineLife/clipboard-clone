50d4b4e8f319480b16c789659710d80daa5ae37b
Eric Laursen <ericjlaursen@gmail.com>
2020-08-15 21:03:57 -0400

    init

---
+++ testable/handlers/crashReport.js
@@ -0,0 +1,5 @@
+const crashReportHandler = (req, res) => {
+	return res.status(200).json({})
+}
+
+module.exports.crashReportHandler = crashReportHandler;
---
+++ testable/handlers/exceptions.js
@@ -0,0 +1,5 @@
+const exceptionsHandler = (req,res) => {
+	return res.status(200).json({})
+}
+
+module.exports.exceptionsHandler = exceptionsHandler;
