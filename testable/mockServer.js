c34fdd2d049545791c0a57b0d09c60c073de1593
Eric Laursen <ericjlaursen@gmail.com>
2020-08-15 20:56:06 -0400

    running server + port

--- testable/mockServer.js
+++ testable/mockServer.js
@@ -1,3 +1,4 @@
+// Dependencies
 const express = require('express');
 const multer = require('multer');
 const bp = require('body-parser');
@@ -5,3 +6,17 @@ const uuid = require('uuid');
 const wf = require('write-file');
 const path = require('path')
 const http = require('http');
+
+// Vars
+const SERVER_PORT = 8082;
+
+
+const app = express();
+const httpServer = http.createServer(app);
+
+// body-parser middleware
+app.use(bp.urlencoded({extended:false}));
+
+httpServer.listen(SERVER_PORT, () => {
+	console.log('crash-server running');
+});
